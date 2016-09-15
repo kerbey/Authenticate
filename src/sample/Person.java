df8a8e10a56c8c930ae1208e617812ff33bd6c94
kerbey <kerbchevalier@verizon.net>
2016-09-14 20:09:00 -0400

    Update Person.java

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -79,58 +79,60 @@ public class Person //extends Application
     {
         System.out.println();
         System.out.println();
-        String passedInLine=text+","+genderBoxText+","+dobBoxText+","+SSNBoxText+","+lastNameBoxText+",";
+        String passedInLine = text + "," + genderBoxText + "," + dobBoxText + "," + SSNBoxText + "," + lastNameBoxText + ",";
         System.out.println(" Person Compare method");
 
         String File = "LoginInformation.txt";
         String line = "";
 
         Scanner scanFile = Controller.createTextRead(File);// scan a file
-        line = scanFile.nextLine();
-        System.out.println("line has ===="+line);
-        System.out.println(line.length());
+        //for(int r=0;r<Controller.count;r++) {
+            line = scanFile.nextLine();
+            System.out.println("line has ====" + line);
+            System.out.println(line.length());
 
 
-        String newLine="";
-        int Count=0;
-        for(int n=0;n<passedInLine.length();n++)//scan line that was passed in to count the number of commas
-        {
-            if(passedInLine.substring(n,n+1).contains(","))
+            String newLine = "";
+            int Count = 0;
+            for (int n = 0; n < passedInLine.length(); n++)//scan line that was passed in to count the number of commas
             {
-                Count++;
-                System.out.println(Count+" commas in the String that got passed in");
+                if (passedInLine.substring(n, n + 1).contains(",")) {
+                    Count++;
+                    //System.out.println(Count+" commas in the String that got passed in");
+                }
             }
-        }
 
-        int commaCount=0;
-        for(int t=0;t<line.length();t++)//this line counts the number of commas found in the line of the scanned file
-        {
-            if(line.substring(t,t+1).contains(","))//if there's a comma count if and also add it to the new string
+            int commaCount = 0;
+            for (int t = 0; t < line.length(); t++)//this line counts the number of commas found in the line of the scanned file
             {
-                newLine+=line.substring(t,t+1);
-                //System.out.println(newLine+"= newLine");
-                commaCount++;
-                //System.out.println(commaCount+" commas in the String from the file");
+                if (line.substring(t, t + 1).contains(","))//if there's a comma count if and also add it to the new string
+                {
+                    newLine += line.substring(t, t + 1);
+                    //System.out.println(newLine+"= newLine");
+                    commaCount++;
+                    //System.out.println(commaCount+" commas in the String from the file");
+                } else if (!line.substring(t, t + 1).contains(",")) {//add other found characters into the string
+                    newLine += line.substring(t, t + 1);
+                    //System.out.println(newLine+"= newLine");
+                }
+                //System.out.println("newLine=="+newLine);
+                if (newLine.contains(passedInLine) && Count == commaCount) {//if the strings and number of commas in each string match stop the loop and compare the two
+                    t = line.length();
+                    System.out.println("both strings you're comparing are equal");
+                }
             }
-            else if(!line.substring(t,t+1).contains(","))
-            {//add other found characters into the string
-                newLine+=line.substring(t,t+1);
-                //System.out.println(newLine+"= newLine");
-            }
-            //System.out.println("newLine=="+newLine);
-            if(newLine.equals(passedInLine)&&Count==commaCount)
-            {//if the strings and number of commas in each string match stop the loop and compare the two
-                t=line.length();
-                System.out.println("both strings you're comparing are equal");
-            }
-        }
-        scanFile.close();
-        System.out.println();
-        System.out.println();
-        if(newLine.equals(passedInLine)&&Count==commaCount)
-            return true;
-        else
-            return false;
+
+            System.out.println("newLine==" + newLine);
+            System.out.println("passedInLine==" + passedInLine);
+            scanFile.close();
+            System.out.println();
+            System.out.println();
+            if (newLine.contains(passedInLine) && Count == commaCount)
+                return true;
+            else
+                return false;
+        //}
+        //return false;
     }
 
 
